b695add9ee3f265bc889ef85e10c4e5c504eb29f
KarthickJoyous <karthikjharris@gmail.com>
2024-02-03 22:14:36 +0530

    Base Layout Setup

---
+++ resources/views/users/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.user.app')
+
+@section('title', 'Home')
+
+@section('breadcrumn')
+	<li class="breadcrumb-item active">Home</li>
+@endsection
+
+@section('content')
+<div class="row">
+	
+</div>
+@endsection
