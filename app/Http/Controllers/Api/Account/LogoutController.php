da974da7e9480ba9596c8cf4d68d04b96151cd7f
KarthickJoyous <karthikjharris@gmail.com>
2024-03-31 17:36:13 +0530

    Logout API Added

--- app/Http/Controllers/Api/Account/LogoutController.php
+++ app/Http/Controllers/Api/Account/LogoutController.php
@@ -3,23 +3,19 @@
 namespace App\Http\Controllers\Api\Account;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\{Request, RedirectResponse};
+use Illuminate\Http\{Request, JsonResponse};
 
 class LogoutController extends Controller
 {
     /**
      * Log the user out of the application.
      * @param Request $request
-     * @return RedirectResponse
+     * @return JsonResponse
      */
-    public function __invoke(Request $request): RedirectResponse
+    public function __invoke(Request $request): JsonResponse
     {
-        auth('web')->logout();
- 
-        $request->session()->invalidate();
-     
-        $request->session()->regenerateToken();
+        $request->user()->currentAccessToken()->delete();
 
-        return redirect()->route('user.login')->with('success', __('messages.user.logout.logout_success'));
+        return $this->success(__('messages.user.logout.logout_success'), []);
     }
 }
